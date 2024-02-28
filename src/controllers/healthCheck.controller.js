8cd9a649276d6d3c803536acf2989546086e37a9
Rohan Gope <rg2512@it.jgec.ac.in>
2024-02-29 02:39:10 +0530

    Add image upload functionality to health check controller

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,10 +1,19 @@
-
+import {ApiResponse} from '../utils/ApiResponse.util.js'
+import {ApiError} from '../utils/ApiError.util.js'
 
 const healthCheckControllerGet = (req, res) => {
   res.status(200).send('Server is up and running');
 };
 
 const healthCheckControllerPost = (req, res) => {
+
+  const file = req.file;
+  if(!file){
+    throw new ApiError(400, 'Please upload an image');
+  }
+  res
+  .status(200)
+  .json(new ApiResponse(200,file.originalname, 'Health check image uploaded successfully'));
   
 };
 
--- src/routes/healthCheck.routes.js
+++ src/routes/healthCheck.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { healthCheckControllerGet,healthCheckControllerPost } from "../controllers/healthCheck.controller.js";
-
-
+import { upload } from "../middleware/multer.middleware.js"; 
 const router = Router();
 
 router
 .get('/',healthCheckControllerGet)
-.post('/',healthCheckControllerPost);
+.post('/',upload.single("http://localhost:8000/api/v1/health") ,healthCheckControllerPost);
 
 
 export default router;
