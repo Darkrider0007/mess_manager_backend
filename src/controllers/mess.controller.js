f7a331c40084e642a7b9bbc35e26e4529e7ca306
Rohan Gope <rg2512@it.jgec.ac.in>
2024-03-05 00:44:40 +0530

    Refactor mess.routes.js file

--- src/controllers/mess.controller.js
+++ src/controllers/mess.controller.js
@@ -1,65 +1,284 @@
-import Mess from '../models/mess.model.js';
-import { ApiError } from '../utils/ApiError.util.js';
-import { asyncHandler } from '../utils/asyncHandler.util.js';
-import fs from 'fs';
-import { uploadImageOnCloudinary } from '../utils/cloudinary.util.js';
-import { ApiResponse } from '../utils/ApiResponse.util.js';
+import Mess from "../models/mess.model.js";
+import { ApiError } from "../utils/ApiError.util.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import fs from "fs";
+import { deleteImageFromCloudinary, uploadImageOnCloudinary } from "../utils/cloudinary.util.js";
+import { ApiResponse } from "../utils/ApiResponse.util.js";
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
 
-const createNewMess =asyncHandler(async (req, res) => {
+const createNewMess = asyncHandler(async (req, res) => {
+  try {
     const { messName, messDescription } = req.body;
-    if(messName.trim() === "" || messDescription.trim() === ""){
-        fs.unlinkSync(req.file?.path);
-        throw new ApiError(400, "Mess Name and Description are required");
+    if (messName.trim() === "" || messDescription.trim() === "") {
+      fs.unlinkSync(req.file?.path);
+      throw new ApiError(400, "Mess Name and Description are required");
     }
 
     const admin = req.user._id;
 
-    const alreadyExist = await Mess.findOne({messName, messAdmin: admin});
-    if(alreadyExist){
-        fs.unlinkSync(req.file?.path);
-        throw new ApiError(400, "Mess already exists");
+    const alreadyExist = await Mess.findOne({ messName, messAdmin: admin });
+    if (alreadyExist) {
+      fs.unlinkSync(req.file?.path);
+      throw new ApiError(400, "Mess already exists");
     }
 
     const messLogo = await uploadImageOnCloudinary(req.file?.path);
 
-    if(!messLogo){
-        fs.unlinkSync(req.file?.path);
-        throw new ApiError(500, "Error while uploading image");
+    if (!messLogo) {
+      fs.unlinkSync(req.file?.path);
+      throw new ApiError(500, "Error while uploading image");
     }
 
     const newMess = await Mess.create({
-        messName,
-        messDescription,
-        messLogo: messLogo.secure_url,
-        messAdmin: admin,
-        messMembers: [admin],
+      messName,
+      messDescription,
+      messLogo: messLogo.secure_url,
+      messAdmin: admin,
+      messMembers: [admin],
     });
 
-    if(!newMess){
-        throw new ApiError(500, "Error while creating new mess");
+    if (!newMess) {
+      throw new ApiError(500, "Error while creating new mess");
     }
 
-    res
-    .status(201)
-    .json(ApiResponse(201,newMess, "Mess Created"));
+    res.status(201).json(ApiResponse(201, newMess, "Mess Created"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
 });
 
 const getMessInfo = asyncHandler(async (req, res) => {
+  try {
     const messId = req.params.messId;
-    if(!messId){
-        throw new ApiError(400, "Mess Id is required");
+    if (!messId) {
+      throw new ApiError(400, "Mess Id is required");
     }
 
-    const mess = await Mess.findById(messId)
+    const mess = await Mess.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(messId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "messMembers",
+          foreignField: "_id",
+          as: "members",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          messName: 1,
+          messDescription: 1,
+          messLogo: 1,
+          totalMoney: 1,
+          messMenu: 1,
+          messAdmin: 1,
+          members: {
+            _id: 1,
+            userName: 1,
+            fullName: 1,
+            email: 1,
+            userAvatar: 1,
+          },
+        },
+      },
+    ]);
 
-    if(!mess){
-        throw new ApiError(404, "Mess not found");
+    if (!mess) {
+      throw new ApiError(404, "Mess not found");
     }
 
-    res
-    .status(200)
-    .json(new ApiResponse(200,mess, "Mess Info"));
+    res.status(200).json(new ApiResponse(200, mess[0], "Mess Info"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
 });
 
+const addMemberToMess = asyncHandler(async (req, res) => {
+  try {
+    const { messId, memberId } = req.body;
+    if (memberId.trim() === "" || messId.trim() === "") {
+      throw new ApiError(400, "Mess Id and Member Id are required");
+    }
 
-export { createNewMess, getMessInfo };
+    const mess = await Mess.findById(messId);
+    if (!mess) {
+      throw new ApiError(404, "Mess not found");
+    }
+
+    if (mess.messAdmin.toString() !== req.user._id.toString()) {
+      throw new ApiError(403, "You are not authorized to add member");
+    }
+
+    const member = await User.findById(memberId);
+    if (!member) {
+      throw new ApiError(404, "Member not found");
+    }
+
+    const alreadyMember = mess.messMembers.find(m => m.toString() === memberId);
+    if (alreadyMember) {
+      throw new ApiError(400, "Member already added");
+    }
+
+    mess.messMembers.push(memberId);
+    await mess.save();
+
+    res
+      .status(200)
+      .json(new ApiResponse(200, { "member ID": memberId }, "Member Added"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+const removeMemberFromMess = asyncHandler(async (req, res) => {
+  const { messId, memberId } = req.body;
+  if (memberId.trim() === "" || messId.trim() === "") {
+    throw new ApiError(400, "Mess Id and Member Id are required");
+  }
+
+  const mess = await Mess.findById(messId);
+  if (!mess) {
+    throw new ApiError(404, "Mess not found");
+  }
+
+  if (mess.messAdmin.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to remove member");
+  }
+
+  const removedMember = mess.messMembers.filter(m => m.toString() !== memberId);
+
+  mess.messMembers = removedMember;
+
+  await mess.save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, { "member ID": memberId }, "Member Removed"));
+});
+
+const getMessMembersInfo = asyncHandler(async (req, res) => {
+  try {
+    const messId = req.params.messId;
+    if (!messId) {
+      throw new ApiError(400, "Mess Id is required");
+    }
+
+    const messMembers = await Mess.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(messId),
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "messMembers",
+          foreignField: "_id",
+          as: "members",
+        },
+      },
+      {
+        $project: {
+          messAdmin: 1,
+          members: {
+            _id: 1,
+            userName: 1,
+            fullName: 1,
+            email: 1,
+            userAvatar: 1,
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json(new ApiResponse(200, messMembers, "Members Info"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
+
+const updateMessInfo = asyncHandler(async (req, res) => {
+    try {
+        const messId = req.params.messId;
+        if (!messId) {
+        throw new ApiError(400, "Mess Id is required");
+        }
+
+        const { messName, messDescription } = req.body;
+
+        if (messName.trim() === "" && messDescription.trim() === "") {
+        throw new ApiError(400, "Mess Name or Description is required");
+        }
+    
+        const updateObject = {};
+        if (messName) {
+            updateObject.messName = messName;
+        }
+        if (messDescription) {
+            updateObject.messDescription = messDescription;
+        }
+
+        const updatedMessInfo = await Mess.findByIdAndUpdate(messId, {
+            $set: updateObject
+        }, { new: true });
+
+        if (!updatedMessInfo) {
+            throw new ApiError(500, "Error while updating mess info");
+        }
+
+        res.status(200).json(new ApiResponse(200, updatedMessInfo, "Mess Info Updated"));
+    } catch (error) {
+        throw new ApiError(500, error.message);
+    }
+});
+
+const updateMessLogo = asyncHandler(async (req, res) => {
+    try {
+        const messId = req.params.messId;
+        if (!messId) {
+        throw new ApiError(400, "Mess Id is required");
+        }
+
+        const mess = await Mess.findById(messId);
+        if (!mess) {
+        throw new ApiError(404, "Mess not found");
+        }
+
+        if (mess.messAdmin.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update mess logo");
+        }
+
+        const messLogo = await uploadImageOnCloudinary(req.file?.path);
+        if (!messLogo) {
+        fs.unlinkSync(req.file?.path);
+        throw new ApiError(500, "Error while uploading image");
+        }
+        const messOldLogo = mess.messLogo;
+
+        mess.messLogo = messLogo.secure_url;
+        
+        await mess.save();
+
+        await deleteImageFromCloudinary(messOldLogo);
+
+        res.status(200).json(new ApiResponse(200, mess, "Mess Logo Updated"));
+    } catch (error) {
+        throw new ApiError(500, error.message);
+    }
+})
+
+export {
+  createNewMess,
+  getMessInfo,
+  addMemberToMess,
+  removeMemberFromMess,
+  getMessMembersInfo,
+  updateMessInfo,
+  updateMessLogo,
+};
--- src/routes/mess.routes.js
+++ src/routes/mess.routes.js
@@ -1,13 +1,26 @@
-import Router from 'express';
-import { verifyJWT } from '../middleware/auth.middleware.js';
-import { upload } from '../middleware/multer.middleware.js';
-import { createNewMess } from '../controllers/mess.controller.js';
-
+import Router from "express";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
+import {
+  addMemberToMess,
+  createNewMess,
+  getMessInfo,
+  getMessMembersInfo,
+  removeMemberFromMess,
+  updateMessInfo,
+  updateMessLogo,
+} from "../controllers/mess.controller.js";
 
 const router = Router();
 
-router.use(verifyJWT)
+router.use(verifyJWT);
 
-router.post('/create-new-mess',upload.single('messLogo'), createNewMess);
+router.post("/create-new-mess", upload.single("messLogo"), createNewMess);
+router.get("/get-mess-info/:messId", getMessInfo);
+router.post("/add-member-to-mess", addMemberToMess);
+router.delete("/remove-member-from-mess", removeMemberFromMess);
+router.get("/get-mess-members-info/:messId", getMessMembersInfo);
+router.patch("/update-mess-info/:messId", updateMessInfo);
+router.patch("/update-mess-logo/:messId", upload.single("messLogo"), updateMessLogo);
 
 export default router;
