9b4eb728b30ecb2ff07633907e88f2cd09993aad
Rohan Gope <rg2512@it.jgec.ac.in>
2024-03-04 23:15:29 +0530

    Add getMessInfo controller function

--- src/controllers/mess.controller.js
+++ src/controllers/mess.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiError.util.js';
 import { asyncHandler } from '../utils/asyncHandler.util.js';
 import fs from 'fs';
 import { uploadImageOnCloudinary } from '../utils/cloudinary.util.js';
+import { ApiResponse } from '../utils/ApiResponse.util.js';
 
 const createNewMess =asyncHandler(async (req, res) => {
     const { messName, messDescription } = req.body;
@@ -31,6 +32,7 @@ const createNewMess =asyncHandler(async (req, res) => {
         messDescription,
         messLogo: messLogo.secure_url,
         messAdmin: admin,
+        messMembers: [admin],
     });
 
     if(!newMess){
@@ -39,12 +41,25 @@ const createNewMess =asyncHandler(async (req, res) => {
 
     res
     .status(201)
-    .json({
-        success: true,
-        data: newMess,
-        message: "Mess created successfully",
-    });
+    .json(ApiResponse(201,newMess, "Mess Created"));
+});
+
+const getMessInfo = asyncHandler(async (req, res) => {
+    const messId = req.params.messId;
+    if(!messId){
+        throw new ApiError(400, "Mess Id is required");
+    }
+
+    const mess = await Mess.findById(messId)
+
+    if(!mess){
+        throw new ApiError(404, "Mess not found");
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,mess, "Mess Info"));
 });
 
 
-export { createNewMess };
+export { createNewMess, getMessInfo };
