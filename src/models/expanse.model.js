8d6236491ec1675359e70c61009de30a0f19a96f
Rohan Gope <rg2512@it.jgec.ac.in>
2024-03-07 02:26:32 +0530

    Add expanse model to project

---
+++ src/models/expanse.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const expanseSchema = new mongoose.Schema({
+    expanseFor : {
+        type: String,
+        required: [true, "Expanse Reason is required"],
+    },
+    messID : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Mess",
+        required: [true, "Mess Id is required"],
+    },
+    amount : {
+        type: Number,
+        default: 0,
+    },
+},{timestamps: true});
+
+expanseSchema.plugin(mongooseAggregatePaginate);
+
+
+export default mongoose.model("Expanse", expanseSchema);
