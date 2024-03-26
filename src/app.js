47e455cec46d06f0239bb985a6a884d6266b6622
Rohan Gope <rg2512@it.jgec.ac.in>
2024-03-26 22:39:48 +0530

    Refactor app.js and healthCheck.controller.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
-)
-
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '16kb'}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.use(cookieParser());
 
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+import userRouter from "./routes/user.routes.js";
+import messRouter from "./routes/mess.routes.js";
+import incomingAmount from "./routes/incomingAmount.routes.js";
+import expanseRouter from "./routes/expanse.routes.js";
 
-import healthCheckRouter  from './routes/healthCheck.routes.js';
-import userRouter from './routes/user.routes.js';
-import messRouter from './routes/mess.routes.js';
-import incomingAmount from './routes/incomingAmount.routes.js';
-import expanseRouter from './routes/expanse.routes.js';
-
-
-app.use('/api/v1/health', healthCheckRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/mess', messRouter);
-app.use('/api/v1/incomingAmount', incomingAmount);
-app.use('/api/v1/expanses', expanseRouter);
+app.use("/api/v1/health", healthCheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/mess", messRouter);
+app.use("/api/v1/incomingAmount", incomingAmount);
+app.use("/api/v1/expanses", expanseRouter);
 
 export { app };
--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,44 +1,52 @@
-import {ApiResponse} from '../utils/ApiResponse.util.js'
-import {ApiError} from '../utils/ApiError.util.js'
-import { asyncHandler } from '../utils/asyncHandler.util.js';
-import fs from 'fs';
-
+import { ApiResponse } from "../utils/ApiResponse.util.js";
+import { ApiError } from "../utils/ApiError.util.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import fs from "fs";
 
 const healthCheckControllerGet = (req, res) => {
-  res.status(200).send('Server is up and running');
+  res.status(200).send("Server is up and running");
 };
 
 const healthCheckControllerPost = (req, res) => {
-
   const file = req.file;
-  if(!file){
-    throw new ApiError(400, 'Please upload an image');
+  if (!file) {
+    throw new ApiError(400, "Please upload an image");
   }
+  const test = "test";
   res
-  .status(200)
-  .json(new ApiResponse(200,file.originalname, 'Health check image uploaded successfully'));
-  
+    .status(200)
+    .cookie("healthCheck", test, {
+      httpOnly: true,
+      secure: true,
+    })
+    .json(
+      new ApiResponse(
+        200,
+        file.originalname,
+        "Health check image uploaded successfully"
+      )
+    );
 };
 
 const healthCheckControllerDelete = asyncHandler(async (req, res) => {
   try {
     const files = fs.readdir("./public/images", (err, files) => {
       if (err) {
-        throw new ApiError(500, 'Error deleting health check images', err);
+        throw new ApiError(500, "Error deleting health check images", err);
       }
       files.map(file => fs.unlinkSync(`./public/images/${file}`));
-      
     });
 
     res
-    .status(200)
-    .json(new ApiResponse(200, 'Health check images deleted successfully'));
-
+      .status(200)
+      .json(new ApiResponse(200, "Health check images deleted successfully"));
   } catch (err) {
-    throw new ApiError(500, 'Error deleting health check images', err);
+    throw new ApiError(500, "Error deleting health check images", err);
   }
 });
 
-
-
-export { healthCheckControllerGet,healthCheckControllerPost,healthCheckControllerDelete };
+export {
+  healthCheckControllerGet,
+  healthCheckControllerPost,
+  healthCheckControllerDelete,
+};
