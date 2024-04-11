d1c33735534bdaf94025c2511831bca7e5a2711f
Rohan Gope <rg2512@it.jgec.ac.in>
2024-04-11 15:54:14 +0530

    Refactor mess.controller.js for better code readability and maintainability

--- src/controllers/mess.controller.js
+++ src/controllers/mess.controller.js
@@ -2,7 +2,10 @@ import Mess from "../models/mess.model.js";
 import { ApiError } from "../utils/ApiError.util.js";
 import { asyncHandler } from "../utils/asyncHandler.util.js";
 import fs from "fs";
-import { deleteImageFromCloudinary, uploadImageOnCloudinary } from "../utils/cloudinary.util.js";
+import {
+  deleteImageFromCloudinary,
+  uploadImageOnCloudinary,
+} from "../utils/cloudinary.util.js";
 import { ApiResponse } from "../utils/ApiResponse.util.js";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
@@ -201,81 +204,87 @@ const getMessMembersInfo = asyncHandler(async (req, res) => {
       },
     ]);
 
-    res.status(200).json(new ApiResponse(200, messMembers, "Members Info"));
+    res.status(200).json(new ApiResponse(200, messMembers[0], "Members Info"));
   } catch (error) {
     throw new ApiError(500, error.message);
   }
 });
 
 const updateMessInfo = asyncHandler(async (req, res) => {
-    try {
-        const messId = req.params.messId;
-        if (!messId) {
-        throw new ApiError(400, "Mess Id is required");
-        }
-
-        const { messName, messDescription } = req.body;
-
-        if (messName?.trim() === "" && messDescription?.trim() === "") {
-        throw new ApiError(400, "Mess Name or Description is required");
-        }
-    
-        const updateObject = {};
-        if (messName) {
-            updateObject.messName = messName;
-        }
-        if (messDescription) {
-            updateObject.messDescription = messDescription;
-        }
-
-        const updatedMessInfo = await Mess.findByIdAndUpdate(messId, {
-            $set: updateObject
-        }, { new: true });
-
-        if (!updatedMessInfo) {
-            throw new ApiError(500, "Error while updating mess info");
-        }
-
-        res.status(200).json(new ApiResponse(200, updatedMessInfo, "Mess Info Updated"));
-    } catch (error) {
-        throw new ApiError(500, error.message);
+  try {
+    const messId = req.params.messId;
+    if (!messId) {
+      throw new ApiError(400, "Mess Id is required");
     }
+
+    const { messName, messDescription } = req.body;
+
+    if (messName?.trim() === "" && messDescription?.trim() === "") {
+      throw new ApiError(400, "Mess Name or Description is required");
+    }
+
+    const updateObject = {};
+    if (messName) {
+      updateObject.messName = messName;
+    }
+    if (messDescription) {
+      updateObject.messDescription = messDescription;
+    }
+
+    const updatedMessInfo = await Mess.findByIdAndUpdate(
+      messId,
+      {
+        $set: updateObject,
+      },
+      { new: true }
+    );
+
+    if (!updatedMessInfo) {
+      throw new ApiError(500, "Error while updating mess info");
+    }
+
+    res
+      .status(200)
+      .json(new ApiResponse(200, updatedMessInfo, "Mess Info Updated"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
 });
 
 const updateMessLogo = asyncHandler(async (req, res) => {
-    try {
-        const messId = req.params.messId;
-        if (!messId) {
-        throw new ApiError(400, "Mess Id is required");
-        }
-
-        const mess = await Mess.findById(messId);
-        if (!mess) {
-        throw new ApiError(404, "Mess not found");
-        }
-
-        if (mess.messAdmin.toString() !== req.user._id.toString()) {
-        throw new ApiError(403, "You are not authorized to update mess logo");
-        }
-
-        const messLogo = await uploadImageOnCloudinary(req.file?.path);
-        if (!messLogo) {
-        fs.unlinkSync(req.file?.path);
-        throw new ApiError(500, "Error while uploading image");
-        }
-        const messOldLogo = mess.messLogo;
-
-        mess.messLogo = messLogo.secure_url;
-        
-        await mess.save();
-
-        await deleteImageFromCloudinary(messOldLogo);
-
-        res.status(200).json(new ApiResponse(200, mess, "Mess Logo Updated"));
-    } catch (error) {
-        throw new ApiError(500, error.message);
+  try {
+    const messId = req.params.messId;
+    if (!messId) {
+      throw new ApiError(400, "Mess Id is required");
     }
-})
+
+    const mess = await Mess.findById(messId);
+    if (!mess) {
+      throw new ApiError(404, "Mess not found");
+    }
+
+    if (mess.messAdmin.toString() !== req.user._id.toString()) {
+      throw new ApiError(403, "You are not authorized to update mess logo");
+    }
+
+    const messLogo = await uploadImageOnCloudinary(req.file?.path);
+    if (!messLogo) {
+      fs.unlinkSync(req.file?.path);
+      throw new ApiError(500, "Error while uploading image");
+    }
+    const messOldLogo = mess.messLogo;
+
+    mess.messLogo = messLogo.secure_url;
+
+    await mess.save();
+
+    await deleteImageFromCloudinary(messOldLogo);
+
+    res.status(200).json(new ApiResponse(200, mess, "Mess Logo Updated"));
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
 
 const updateMessAdmin = asyncHandler(async (req, res) => {
   try {
@@ -303,9 +312,11 @@ const updateMessAdmin = asyncHandler(async (req, res) => {
       throw new ApiError(403, "You are not authorized to change admin");
     }
 
-    const isMemberOfTheMess = mess.messMembers.find(m => m.toString() === newAdminId);
+    const isMemberOfTheMess = mess.messMembers.find(
+      m => m.toString() === newAdminId
+    );
 
-    if(!isMemberOfTheMess) {
+    if (!isMemberOfTheMess) {
       throw new ApiError(400, "New Admin is not a member of the mess");
     }
 
@@ -343,9 +354,8 @@ const addMessMenu = asyncHandler(async (req, res) => {
     await mess.save();
 
     res.status(200).json(new ApiResponse(200, mess, "Menu Added"));
-    
   } catch (error) {
-    throw new ApiError(500, error.message);    
+    throw new ApiError(500, error.message);
   }
 });
 
@@ -375,9 +385,8 @@ const removeMessMenu = asyncHandler(async (req, res) => {
     await mess.save();
 
     res.status(200).json(new ApiResponse(200, mess, "Menu Removed"));
-    
   } catch (error) {
-    throw new ApiError(500, error.message);    
+    throw new ApiError(500, error.message);
   }
 });
 
